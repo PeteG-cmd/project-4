54a14e0498f84b587c7eef1db533f6e63bb41fe8
Peter Gilbert <peter.gilbert88@icloud.com>
2020-06-09 22:57:23 +0100

    Photo links update

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -150,7 +150,7 @@ export default class Home extends React.Component {
             <div>
               <div className='circleBox'>
                 <p className="image is-100x100">
-                  <img className='is-rounded' src={'http://localhost:4000/media/assets/round-logo-1.jpg'}></img>
+                  <img className='is-rounded' src={'/media/assets/round-logo-1.jpg'}></img>
                 </p>
               </div>
               <div>
@@ -163,7 +163,7 @@ export default class Home extends React.Component {
               <div className='circleBox'>
                 <Link to={'/addProfileImage'}>
                   <p className="image is-100x100">
-                    {user.image && <img className='is-rounded' src={`http://localhost:4000${user.image}`}></img>}
+                    {user.image && <img className='is-rounded' src={`${user.image}`}></img>}
                     {!user.image && <img className='is-rounded' src={'/media/assets/user-placeholder.jpg'}></img>}
                   </p>
                 </Link>
